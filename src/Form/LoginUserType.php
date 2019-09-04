1fa7da8a2844843547f71d5a6f324f7b364c6b7e
ZinebElam <zineb.elamrani96@gmail.com>
2019-09-04 15:45:05 +0200

    update conference controller

--- src/Form/LoginUserType.php
+++ src/Form/LoginUserType.php
@@ -16,7 +16,7 @@ class LoginUserType extends AbstractType
         $builder
             ->add('email', EmailType::class)
             ->add('password', PasswordType::class)
-            ->add('Log in', SubmitType::class)
+            ->add('submit', SubmitType::class)
         ;
     }
 
